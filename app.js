c81b08e50f22e6c5c26f09bc7abde5ac5bf399ff
Joe Li <joe.li@okchem.com>
2017-09-24 17:05:51 +0800

    Involve eslint to making code more consistent Add dotenv to do the configuration Add winston and winston-daily-rotate-file to do logging Add nodemon cross-env to set NODE_ENV when start Involve mongoose

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-var express = require('express');
-var path = require('path');
-var logger = require('morgan');
-var bodyParser = require('body-parser');
+const express = require('express');
+const accessLogger = require('morgan');
+require('dotenv').config({path:`./${process.env.NODE_ENV}.env`});
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const index = require('./routes/index');
+const users = require('./routes/users');
+const logger = require('./utils/logger')
+const app = express();
 
-var index = require('./routes/index');
-var users = require('./routes/users');
-
-var app = express();
-
-app.use(logger('dev'));
+app.use(accessLogger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -19,16 +19,20 @@ app.use('/users', users);
 app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
-  err.code = "404";
-  err.message = "Request resource doesn't exist"
+  err.code = '404';
+  err.message = 'Request resource doesn\'t exist';
   next(err);
 });
 
 // error handler
-app.use(function (err, req, res, next) {
+app.use(function (err, req, res) {
   // render the error page
   res.status(err.status || 500);
-  res.json({ code: err.code || 500, message: err.message })
+  res.json({ code: err.code || 500, message: err.message });
+});
+
+mongoose.connect('mongodb://localhost/emmab', { useMongoClient: true }, function(error) {
+  logger.error(error.message)
 });
 
 module.exports = app;
